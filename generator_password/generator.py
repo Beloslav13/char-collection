619665c7ca97b2630b9fec4ad2a7b9fb98638519
beloslav <velislav1329@gmail.com>
2020-06-20 18:50:03 +0300

    Add docs and property

--- generator_password/generator.py
+++ generator_password/generator.py
@@ -2,6 +2,9 @@ import random
 
 
 class GeneratorPassword:
+    """
+    A class generating a random password of different characters.
+    """
     SPECIAL_CHARACTERS = list('!@#$%^&*()_=+[]{};:"\|,')
     CONTINUE_CHR = list(range(91, 97))
     ALPHABET_CHR = range(65, 123)
@@ -9,7 +12,11 @@ class GeneratorPassword:
     def __init__(self):
         self.all_characters = []
 
+    @property
     def _collect_char(self):
+        """
+        Internal method returning a list of all characters.
+        """
         for i in self.ALPHABET_CHR:
             if i in self.CONTINUE_CHR:
                 continue
@@ -17,8 +24,14 @@ class GeneratorPassword:
         self.all_characters.extend(self.SPECIAL_CHARACTERS)
         return self.all_characters
 
+    # TODO: возможно стоит сделать не статический выбор по длинне пароля, а давать пользователю выбирать длину < 20.
     def generator(self, count=6):
-        collect_chars = self._collect_char()
+        """
+        Password generator returning a password string.
+        :param count: int (6, 8 or 12)
+        :return: '' <= random string of characters
+        """
+        collect_chars = self._collect_char
         random.shuffle(collect_chars)
         if count == 6:
             random.shuffle(collect_chars)
@@ -39,4 +52,3 @@ if __name__ == '__main__':
     print('Second password:', generator.generator(8))
     print('Third password:', generator.generator(12))
     print('Error checking', generator.generator(13))
-
