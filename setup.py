71591bf268fb75065618083bac93f9c030055432
beloslav <velislav1329@gmail.com>
2020-06-24 14:42:11 +0300

    up version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="char-collection",
-    version="0.1.0",
+    version="1.0",
     author="Beloslav",
     author_email="velislav1329@gmail.com",
     description="Building random characters",
