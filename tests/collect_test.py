b55c2e01a42391180d7095213a9a059c65b35604
beloslav <velislav1329@gmail.com>
2021-03-06 14:34:04 +0300

    fix tests...

--- tests/collect_test.py
+++ tests/collect_test.py
@@ -8,33 +8,35 @@ class MyCollectCharTest(unittest.TestCase):
     def test_normal(self):
         collect = CharacterSequence()
         result = collect.collect(10)
-        self.assertEqual(len(result), 10, 'Unexpected length returned (10)')
+        self.assertEqual(len(result), 10, "Unexpected length returned (10)")
 
     def test_zero(self):
         collect = CharacterSequence()
         result = collect.collect(0)
-        self.assertEqual(result, None, 'in test_zero return not None')
+        self.assertEqual(result, "ValuerError: length can be from 1 to 75 characters", "in test_zero return not valid")
 
     def test_big(self):
         collect = CharacterSequence()
         result = collect.collect(999)
-        self.assertEqual(result, None, 'in test big return not None')
+        self.assertEqual(result, "ValuerError: length can be from 1 to 75 characters", "in test big return not valid")
 
     def test_negative(self):
         collect = CharacterSequence()
         result = collect.collect(-1)
-        self.assertEqual(result, None, 'in test negative return not None')
+        self.assertEqual(result, "ValuerError: length can be from 1 to 75 characters",
+                         "in test negative return not valid")
 
     def test_string(self):
         collect = CharacterSequence()
-        result = collect.collect('dsds')
-        self.assertEqual(result, None, 'in test string return not None')
+        result = collect.collect("dsds")
+        self.assertEqual(result, "ValuerError: invalid literal for int() with base 10: 'dsds'",
+                         "in test string return not valid")
 
     def test_int_string(self):
         collect = CharacterSequence()
-        result = collect.collect('10')
-        self.assertEqual(len(result), 10, 'in test int string return not None')
+        result = collect.collect("10")
+        self.assertEqual(len(result), 10, "in test int string return not valid")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
